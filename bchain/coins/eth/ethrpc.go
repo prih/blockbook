3fd5e6640fc989aeb27d83c502455f90de509b4d
prih <prih@prihhost.com>
2021-03-09 14:36:16 +0200

    .

--- bchain/coins/eth/ethrpc.go
+++ bchain/coins/eth/ethrpc.go
@@ -335,19 +335,19 @@ func (b *EthereumRPC) GetChainInfo() (*bchain.ChainInfo, error) {
 	if err != nil {
 		return nil, err
 	}
-	var ver, protocol string
+	var ver string
 	if err := b.rpc.CallContext(ctx, &ver, "web3_clientVersion"); err != nil {
 		return nil, err
 	}
-	if err := b.rpc.CallContext(ctx, &protocol, "eth_protocolVersion"); err != nil {
-		return nil, err
-	}
+	// if err := b.rpc.CallContext(ctx, &protocol, "eth_protocolVersion"); err != nil {
+	// 	return nil, err
+	// }
 	rv := &bchain.ChainInfo{
 		Blocks:          int(h.Number.Int64()),
 		Bestblockhash:   h.Hash().Hex(),
 		Difficulty:      h.Difficulty.String(),
 		Version:         ver,
-		ProtocolVersion: protocol,
+		ProtocolVersion: "",
 	}
 	idi := int(id.Uint64())
 	if idi == 1 {
